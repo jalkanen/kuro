02914e83880b38a99bbb2d30dd46fd952a8d9f93
Janne Jalkanen <jalkanen@ecyrd.com>
2016-03-01 23:50:28 +0200

    Added Securitymanager code.

--- mgt/securitymanager.go
+++ mgt/securitymanager.go
@@ -3,10 +3,37 @@ package mgt
 import (
 	"github.com/jalkanen/kuro/authc"
 	"github.com/jalkanen/kuro/subject"
+	"github.com/jalkanen/kuro/realm"
 )
 
 type SecurityManager interface {
-	createSubject(context subject.SubjectContext) subject.Subject
-	login(subject.Subject, authc.AuthenticationToken) subject.Subject
-	logout(subject.Subject)
+	CreateSubject(context subject.SubjectContext) subject.Subject
+	Login(subject.Subject, authc.AuthenticationToken) subject.Subject
+	Logout(subject.Subject)
 }
+
+type RealmSecurityManager struct {
+	realms []realm.Realm
+}
+
+// Replaces the realms with a single realm
+func (sm *RealmSecurityManager) SetRealm(r *realm.Realm) {
+	sm.realms = [1]realm.Realm{r}
+}
+
+// Replaces all of the realms.  Makes a copy of the incoming slice so changes to it won't mess with things
+func (sm *RealmSecurityManager) SetRealms(r []realm.Realm) {
+	sm.realms = make([]realm.Realm, len(r), cap(r))
+	copy(sm.realms,r)
+}
+
+// Returns the current Realm list
+func (sm *RealmSecurityManager) Realms() []realm.Realm {
+	return sm.realms
+}
+
+type AuthenticatingSecurityManager struct {
+	RealmSecurityManager
+	authenticator authc.Authenticator
+}
+
