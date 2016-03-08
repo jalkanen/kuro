8ab7087511a101eaa5ca4c6033a50234130df32a
Janne Jalkanen <jalkanen@gmail.com>
2016-03-08 17:05:46 +0200

    Added SimpleAccountRealm

--- realm/realm.go
+++ realm/realm.go
@@ -11,33 +11,42 @@ import (
 	"fmt"
 )
 
+// Realms are essentially user, role and permission databases.
 type Realm interface {
 	AuthenticationInfo(authc.AuthenticationToken) (authc.AuthenticationInfo,error)
 	Name() string
 	Supports(authc.AuthenticationToken) bool
 }
 
+// An AuthenticatingRealm provides authentication capabilities
 type AuthenticatingRealm interface {
 	Realm
 	CredentialsMatcher() credential.CredentialsMatcher
 }
 
+// An AuthorizingRealm provides authorization capabilities by adding a Authorizer.
 type AuthorizingRealm interface {
 	AuthenticatingRealm
 	authz.Authorizer
 }
 
-// Reads the contents from an .ini file
-type IniRealm struct {
+// A simple in-memory realm. Highly performant, but does not reload its contents, so
+// cannot be changed.  Useful for testing.  This is an AuthorizingRealm.
+type SimpleAccountRealm struct {
 	name               string
 	users              map[string]authc.SimpleAccount
 	roles              map[string]authz.SimpleRole
 	credentialsMatcher credential.CredentialsMatcher
 }
 
+// Reads the contents from an .ini file; otherwise this is just a basic SimpleAccountRealm
+type IniRealm struct {
+	SimpleAccountRealm
+}
+
 // Creates a new IniRealm, reading from a Reader.
 func NewIni(name string, in io.Reader) (*IniRealm, error) {
-	realm := IniRealm{name: name}
+	realm := IniRealm{ SimpleAccountRealm{name: name} }
 	realm.users = make(map[string]authc.SimpleAccount)
 	realm.roles = make(map[string]authz.SimpleRole)
 
@@ -90,18 +99,18 @@ func NewIni(name string, in io.Reader) (*IniRealm, error) {
 	return &realm, nil
 }
 
-func (r *IniRealm) Name() string {
+func (r *SimpleAccountRealm) Name() string {
 	return r.name
 }
 
 // Supports only UsernamePasswordTokens
-func (r *IniRealm) Supports(token authc.AuthenticationToken) bool {
+func (r *SimpleAccountRealm) Supports(token authc.AuthenticationToken) bool {
 	_, ok := token.(*authc.UsernamePasswordToken)
 
 	return ok
 }
 
-func (r *IniRealm) AuthenticationInfo(token authc.AuthenticationToken) (authc.AuthenticationInfo, error) {
+func (r *SimpleAccountRealm) AuthenticationInfo(token authc.AuthenticationToken) (authc.AuthenticationInfo, error) {
 	t,_ := token.(*authc.UsernamePasswordToken)
 
 	acct, ok := r.users[t.Username()]
@@ -115,13 +124,13 @@ func (r *IniRealm) AuthenticationInfo(token authc.AuthenticationToken) (authc.Au
 
 // AuthenticatingRealm interface
 
-func (r *IniRealm) CredentialsMatcher() credential.CredentialsMatcher {
+func (r *SimpleAccountRealm) CredentialsMatcher() credential.CredentialsMatcher {
 	return r.credentialsMatcher
 }
 
 // Authorizer interface
 
-func (r *IniRealm) HasRole(principals []interface{}, role string) bool {
+func (r *SimpleAccountRealm) HasRole(principals []interface{}, role string) bool {
 	if len(principals) == 0 {
 		return false
 	}
@@ -131,7 +140,7 @@ func (r *IniRealm) HasRole(principals []interface{}, role string) bool {
 	return ok && acct.HasRole(role)
 }
 
-func (r *IniRealm) IsPermittedP(principals []interface{}, permission authz.Permission) bool {
+func (r *SimpleAccountRealm) IsPermittedP(principals []interface{}, permission authz.Permission) bool {
 	acct, ok := r.users[principals[0].(stringer).String()]
 
 	if ok {
@@ -146,7 +155,7 @@ func (r *IniRealm) IsPermittedP(principals []interface{}, permission authz.Permi
 	return false
 }
 
-func (r *IniRealm) IsPermitted(subjectPrincipal []interface{}, permission string) bool {
+func (r *SimpleAccountRealm) IsPermitted(subjectPrincipal []interface{}, permission string) bool {
 	p, err := authz.NewWildcardPermission(permission)
 
 	if err != nil {
