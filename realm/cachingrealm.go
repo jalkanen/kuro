fe2f0470287e14743d2e2e2c839fde23371ecac1
Janne Jalkanen <jalkanen@ecyrd.com>
2016-04-13 22:58:15 +0300

    Implemented a bit more cachingrealm.

--- realm/cachingrealm.go
+++ realm/cachingrealm.go
@@ -2,6 +2,9 @@ package realm
 
 import (
 	"github.com/jalkanen/kuro/cache"
+	"github.com/jalkanen/kuro/authc"
+	"github.com/jalkanen/kuro/authc/credential"
+	"time"
 )
 
 /*
@@ -11,19 +14,64 @@ import (
 	The Cache is configurable.
  */
 type CachingRealm struct {
+	// How long do we cache the AuthenticationInfo objects? Default is 60 seconds.
+	AuthenticationAge time.Duration
+
+	// How long do we store the AuthorizationInfo objects? Default is 60 seconds.
+	AuthorizationAge  time.Duration
+
 	realm AuthorizingRealm
 	cache cache.Cache
 }
 
+// Creates a new cachingrealm based on a backing realm and a cache instance.
 func NewCaching( realm AuthorizingRealm, cache cache.Cache ) *CachingRealm {
 	return &CachingRealm{
 		realm : realm,
 		cache : cache,
+		AuthenticationAge: 60*time.Second,
+		AuthorizationAge: 60*time.Second,
 	}
 }
 
 // Returns "CachingRealm(the backing realm name)".
 func (r *CachingRealm) Name() string {
 	return "CachingRealm("+r.realm.Name()+")"
+}
+
+func (r *CachingRealm) AuthenticationInfo(token authc.AuthenticationToken) (authc.AuthenticationInfo,error) {
+	cachekey, ok := token.Principal().(string)
+	var info authc.AuthenticationInfo
+	var err error
+
+	if ok {
+		info = r.cache.Get(cachekey).(authc.AuthenticationInfo)
+
+		if info != nil {
+			return info, nil
+		}
+	}
+
+	info, err = r.realm.AuthenticationInfo(token)
+
+	if err != nil {
+		// TODO: Should also cache the negative result.
+		return nil, err
+	}
+
+	if ok {
+		r.cache.Set(cachekey, cache.Item{Maxage: r.AuthenticationAge, Value: info})
+	}
+
+	return info, nil
+}
+
+// This is not cached, but passed through to the actual backing realm.
+func (r *CachingRealm) Supports(token authc.AuthenticationToken) bool {
+	return r.realm.Supports(token)
+}
+
+func (r *CachingRealm) CredentialsMatcher() credential.CredentialsMatcher {
+	return r.realm.CredentialsMatcher()
 }
 
